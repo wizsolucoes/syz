dc5b688c5dee0a39f7efc5845298ef59eb5fee77
Tamara Montijo <tamaramontijo@wizsolucoes.com.br>
2021-06-23 18:36:13 -0300

    chore: improve login documentation

--- packages/ng-syz/projects/app/src/app/features/components/docs-login-with-carousel-flow/docs-login-with-carousel-flow.component.ts
+++ packages/ng-syz/projects/app/src/app/features/components/docs-login-with-carousel-flow/docs-login-with-carousel-flow.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ComponentProps } from '../../../shared/models/component-props.interface';
 
 @Component({
   selector: 'app-docs-login-with-carousel-flow',
@@ -6,8 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./docs-login-with-carousel-flow.component.scss'],
 })
 export class DocsLoginWithCarouselFlowComponent {
+  importCode = `
+  import { NgSyzLoginWithCarouselFlowModule } from '@wizsolucoes/ng-syz';';
+  `;
+
+  componentCSSVariables: ComponentProps[] = [
+    {
+      name: `--syz-login-with-carousel-content-login-background-color:`,
+      description: 'Cor de fundo do conteúdo de login',
+    },
+    {
+      name: `--syz-login-with-carousel-slide-background-color`,
+      description: 'Cor de fundo do container com slides',
+    },
+    {
+      name: `--syz-login-with-carousel-flow-button-color`,
+      description: 'Cor dos botões',
+    },
+    {
+      name: `--syz-login-with-carousel-flow-button-color-dark`,
+      description: 'Cor secundária para o hover',
+    },
+  ];
+
   htmlExampleCode = `
-  <ng-syz-login-with-carousel-flow></ng-syz-login-with-carousel-flow>
+  <ng-syz-login-with-carousel-flow
+      [productLogo]="exampleProductLogo"
+      [slides]="exampleSlides"
+      [signUpPath]="'/components/examples/login-with-carousel-flow-example-one'"
+      [forgotPasswordPath]="
+          '/components/examples/login-with-carousel-flow-example-one'
+      "
+  ></ng-syz-login-with-carousel-flow>
   `;
 
   cssExample1Code = `
