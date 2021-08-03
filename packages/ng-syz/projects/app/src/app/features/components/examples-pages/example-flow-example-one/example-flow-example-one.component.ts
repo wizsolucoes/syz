43233b66ffe5d1b481b178d8a7577586fdc53c2d
TamaraMontijo <56484115+TamaraMontijo@users.noreply.github.com>
2021-08-03 11:56:33 -0300

    chore(ng-syz): remove example pages
    Co-authored-by: Tamara Montijo <tamaramontijo@wizsolucoes.com.br>

--- packages/ng-syz/projects/app/src/app/features/components/examples-pages/example-flow-example-one/example-flow-example-one.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-example-flow-example-one',
-  templateUrl: './example-flow-example-one.component.html',
-  styleUrls: ['./example-flow-example-one.component.scss'],
-})
-export class ExampleFlowExampleOneComponent {}
--- packages/ng-syz/projects/app/src/app/features/components/examples-pages/example-flow-example-two/example-flow-example-two.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-example-flow-example-two',
-  templateUrl: './example-flow-example-two.component.html',
-  styleUrls: ['./example-flow-example-two.component.scss'],
-})
-export class ExampleFlowExampleTwoComponent {}
