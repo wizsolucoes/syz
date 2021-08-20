732999e62b8226fd260cb3530186fe79937e387c
Fábio Henrique Alves <fabioalves@wizsolucoes.com.br>
2021-08-20 16:29:26 -0300

    feat: doc component

--- packages/ng-syz/projects/app/src/app/features/components/docs-resultado-simulacao/docs-resultado-simulacao.component.spec.ts
+++ packages/ng-syz/projects/app/src/app/features/components/docs-resultado-simulacao/docs-resultado-simulacao.component.spec.ts
@@ -5,21 +5,49 @@ import { DocsResultadoSimulacaoComponent } from './docs-resultado-simulacao.comp
 describe('DocsResultadoSimulacaoComponent', () => {
   let component: DocsResultadoSimulacaoComponent;
   let fixture: ComponentFixture<DocsResultadoSimulacaoComponent>;
+  let template: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DocsResultadoSimulacaoComponent ]
-    })
-    .compileComponents();
+      declarations: [DocsResultadoSimulacaoComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DocsResultadoSimulacaoComponent);
     component = fixture.componentInstance;
+    template = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a valid description', () => {
+    const description = component.description;
+    expect(description).not.toBeNull();
+  });
+
+  it('should have valid example values when initialize component', () => {
+    component.htmlExampleCode1 = '';
+    component.tsExampleCode1 = '';
+    component.cssExampleCode1 = '';
+
+    const htmlExampleCode1 = component.htmlExampleCode1;
+    const tsExampleCode1 = component.tsExampleCode1;
+    const cssExampleCode1 = component.cssExampleCode1;
+
+    expect(htmlExampleCode1).toEqual('');
+    expect(tsExampleCode1).toEqual('');
+    expect(cssExampleCode1).toEqual('');
+
+    component.initExampleVariables();
+
+    expect(htmlExampleCode1).not.toBeNull();
+    expect(tsExampleCode1).not.toBeNull();
+    expect(cssExampleCode1).not.toBeNull();
+  });
+
+  it('shoud call enviarCardSelecionado when card is clicked', () => {});
 });
--- packages/ng-syz/projects/app/src/app/features/components/docs-resultado-simulacao/docs-resultado-simulacao.module.ts
+++ packages/ng-syz/projects/app/src/app/features/components/docs-resultado-simulacao/docs-resultado-simulacao.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DocsResultadoSimulacaoRoutingModule } from './docs-resultado-simulacao-routing.module';
+import { SharedModule } from '../../../shared/shared.module';
 import { DocsResultadoSimulacaoComponent } from './docs-resultado-simulacao.component';
 import { NgSyzResultadoSimulacaoModule } from 'projects/ng-syz/src/public-api';
-import { SharedModule } from '../../../shared/shared.module';
 
 @NgModule({
   declarations: [DocsResultadoSimulacaoComponent],
