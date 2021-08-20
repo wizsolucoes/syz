0711a753a154b9b61990132b0ec38c0f80b40d2c
Iago Cortes <iagocortes@gmail.com>
2021-08-20 13:19:39 -0300

    feat: simulação resultado parcial

--- packages/ng-syz/projects/ng-syz/src/lib/resultado-simulacao/resultado-simulacao.component.ts
+++ packages/ng-syz/projects/ng-syz/src/lib/resultado-simulacao/resultado-simulacao.component.ts
@@ -6,33 +6,82 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./resultado-simulacao.component.scss'],
 })
 export class NgSyzResultadoSimulacaoComponent {
-  @Input() informacoes: InputInformacoes = {
-    valor: 2500000,
-    modalidades: ['Price', 'SAC', 'SACRE'],
-    condicoes: [
-      {
-        condicao: 'CESH',
-        valor: '10,21%',
-        ajuda: 'Ajuda',
-      },
-      {
-        condicao: 'CET',
-        valor: '15,35%',
-        ajuda: 'Ajuda',
-      },
-      {
-        condicao: 'Valor bruto',
-        valor: 'R$ 260.250,00',
-        ajuda: 'Ajuda',
-      },
-    ],
-  };
+  @Input() ajudaModalidades: string = 'Ajuda';
+  @Input() cards: Card[] = [
+    {
+      prazo: 60,
+      parcelaInicial: 7040.23,
+      parcelaFinal: 4452.83,
+      rendaMinima: 21120.69,
+    },
+    {
+      prazo: 87,
+      parcelaInicial: 4752.09,
+      parcelaFinal: 4702.22,
+      rendaMinima: 15840.29,
+    },
+    {
+      prazo: 60,
+      parcelaInicial: 7040.23,
+      parcelaFinal: 4452.83,
+      rendaMinima: 21120.69,
+    },
+    {
+      prazo: 87,
+      parcelaInicial: 4752.09,
+      parcelaFinal: 4702.22,
+      rendaMinima: 15840.29,
+    },
+    {
+      prazo: 60,
+      parcelaInicial: 7040.23,
+      parcelaFinal: 4452.83,
+      rendaMinima: 21120.69,
+    },
+    {
+      prazo: 87,
+      parcelaInicial: 4752.09,
+      parcelaFinal: 4702.22,
+      rendaMinima: 15840.29,
+    },
+    {
+      prazo: 60,
+      parcelaInicial: 7040.23,
+      parcelaFinal: 4452.83,
+      rendaMinima: 21120.69,
+    },
+  ];
+  @Input() valor: number = 2500000;
+  @Input() modalidades: string[] = ['Price', 'SAC', 'SACRE'];
+  @Input() condicoes: Condicao[] = [
+    {
+      condicao: 'CESH',
+      valor: '10,21%',
+      ajuda: 'Ajuda',
+    },
+    {
+      condicao: 'CET',
+      valor: '15,35%',
+      ajuda: 'Ajuda',
+    },
+    {
+      condicao: 'Valor bruto',
+      valor: 'R$ 260.250,00',
+      ajuda: 'Ajuda',
+    },
+  ];
+  @Input() linhaSelecionada: linhaSelecionada = 'rendaMinima';
+
+  selecionarCard(event) {
+    var target = event.target || event.srcElement || event.currentTarget;
+  }
 }
 
-interface InputInformacoes {
-  valor: number;
-  modalidades: string[];
-  condicoes: Condicao[];
+interface Card {
+  prazo: number;
+  parcelaInicial: number;
+  parcelaFinal: number;
+  rendaMinima: number;
 }
 
 interface Condicao {
@@ -40,3 +89,5 @@ interface Condicao {
   valor: string;
   ajuda: string;
 }
+
+type linhaSelecionada = 'parcelaInicial' | 'parcelaFinal' | 'rendaMinima';
--- packages/ng-syz/projects/ng-syz/src/lib/resultado-simulacao/resultado-simulacao.module.ts
+++ packages/ng-syz/projects/ng-syz/src/lib/resultado-simulacao/resultado-simulacao.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgSyzResultadoSimulacaoComponent } from './resultado-simulacao.component';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
-import { NgxMaskModule, IConfig } from 'ngx-mask';
-const maskConfigFunction: () => Partial<IConfig> = () => {
-  return {
-    validation: false,
-  };
-};
-
+registerLocaleData(ptBr);
 @NgModule({
   declarations: [NgSyzResultadoSimulacaoComponent],
   imports: [
     CommonModule,
     MatButtonToggleModule,
-    NgxMaskModule.forRoot(maskConfigFunction),
+    MatIconModule,
+    MatTooltipModule,
   ],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt' }],
   exports: [NgSyzResultadoSimulacaoComponent],
 })
 export class NgSyzResultadoSimulacaoModule {}
