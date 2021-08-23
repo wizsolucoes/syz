e212394ab47e811d4fb6b751317c9fbdf00517bd
Gabriel Lennon <gabriellennon@Gabriels-MacBook-Air.local>
2021-08-23 12:31:15 -0300

    cards result icons

--- packages/ng-syz/projects/app/src/app/features/components/docs-card-result-initiative/docs-card-result-initiative.component.ts
+++ packages/ng-syz/projects/app/src/app/features/components/docs-card-result-initiative/docs-card-result-initiative.component.ts
@@ -6,10 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./docs-card-result-initiative.component.scss']
 })
 export class DocsCardResultInitiativeComponent implements OnInit {
+  image = {
+    url: 'https://images-na.ssl-images-amazon.com/images/I/610mHhzQThL.png', 
+    alt: 'Teste'
+  }
+
+  cards = [
+    {
+      icon: 'money',
+      text: 'incentivo financeiro',
+      value: 'R$ 910',
+      color: '#0AB64F'
+    },
+    {
+      icon: 'car',
+      text: 'Seguro auto',
+      value: '16',
+      color: '#14ACE8'
+    },
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  myFunction(event){
+    console.log(event)
   }
 
 }
--- packages/ng-syz/projects/ng-syz/src/lib/card-result-initiative/card-result-initiative.component.ts
+++ packages/ng-syz/projects/ng-syz/src/lib/card-result-initiative/card-result-initiative.component.ts
@@ -1,10 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { Cards, IconsAssets } from './models';
 
 @Component({
   selector: 'ng-syz-card-result-initiative',
   templateUrl: './card-result-initiative.component.html',
   styleUrls: ['./card-result-initiative.component.scss'],
 })
-export class NgSyzCardResultInitiativeComponent {
-  @Input() person: any;
-}
+export class NgSyzCardResultInitiativeComponent implements OnInit {
+  @Input() name: string;
+  @Input() image: any;
+  @Input() title: string;
+  @Input() description: string;
+  @Input() type: string;
+  @Input() cards: Cards[];
+  // @Output() action: EventEmitter<any> = new EventEmitter();
+
+  constructor(){}
+  
+  ngOnInit(): void{
+    console.log(this.image)
+  }
+
+  showIcon(name): string{
+    const iconRender = IconsAssets.filter(e => e.icon === name)
+    console.log(iconRender)
+    return iconRender.length ? iconRender[0].svg : '';
+  }
+} 
