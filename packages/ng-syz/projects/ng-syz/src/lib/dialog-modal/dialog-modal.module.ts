74cd6ee0cea25a8fb307884e3de3f79b72ff8612
domingospereira <dpsnbsb79@gmail.com>
2021-08-26 10:32:42 -0300

    chore: declaração component

--- packages/ng-syz/projects/ng-syz/src/lib/dialog-modal/dialog-modal.module.ts
+++ packages/ng-syz/projects/ng-syz/src/lib/dialog-modal/dialog-modal.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgSyzDialogModalComponent } from './dialog-modal.component';
 import { MatDialogModule } from '@angular/material/dialog';
-
+import { DialogModalBodyComponent } from './dialog-modal-body/dialog-modal-body.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { MatDialogModule } from '@angular/material/dialog';
   ],
   imports: [
     CommonModule,
-    MatDialogModule
+    MatDialogModule,
+    DialogModalBodyComponent
   ],
   exports: [NgSyzDialogModalComponent],
 })
