73dbcb6078da78c9e9b576b39ed249d1466bc167
Fábio Henrique Alves <fabioalves@wizsolucoes.com.br>
2021-08-26 18:06:10 -0300

    test: cotagro button grid

---
+++ packages/ng-syz/projects/app/src/app/features/components/docs-button-grid/docs-button-grid.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgSyzButtonGridModule } from 'projects/ng-syz/src/public-api';
+import { SharedModule } from '../../../shared/shared.module';
+
+import { DocsButtonGridComponent } from './docs-button-grid.component';
+
+describe('DocsButtonGridComponent', () => {
+  let component: DocsButtonGridComponent;
+  let fixture: ComponentFixture<DocsButtonGridComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        NgSyzButtonGridModule,
+        RouterTestingModule,
+        SharedModule,
+        NoopAnimationsModule,
+      ],
+      declarations: [DocsButtonGridComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DocsButtonGridComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ packages/ng-syz/projects/ng-syz/src/lib/button-grid/button-grid.component.spec.ts
@@ -0,0 +1,82 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { NgSyzButtonGridComponent } from './button-grid.component';
+
+describe('NgSyzButtonGridComponent', () => {
+  let component: NgSyzButtonGridComponent;
+  let fixture: ComponentFixture<NgSyzButtonGridComponent>;
+  let template: HTMLElement;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NgSyzButtonGridComponent],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        MatButtonModule,
+        MatInputModule,
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NgSyzButtonGridComponent);
+    component = fixture.componentInstance;
+    template = fixture.nativeElement;
+    spyOn(component, 'initCssVariables');
+
+    component.titulo = 'Tipo de Seguro';
+    component.subtitulo = 'Subtítulo';
+    component.buttons = [
+      { text: 'Seguro Cafezal', iconClass: 'fa fa-coffee', link: '#' },
+      { text: 'Seguro Pecuário', iconClass: 'fa fa-eercast', link: '#' },
+      { text: 'Seguro Canavial', iconClass: 'fa fa-picture-o', link: '#' },
+      { text: 'Seguro Floresta', iconClass: 'fa fa-tree', link: '#' },
+    ];
+
+    fixture.detectChanges();
+  });
+
+  it('should create and initialize css variables', () => {
+    expect(component).toBeTruthy();
+    expect(component.initCssVariables).toHaveBeenCalled();
+  });
+
+  it('should display all button values', () => {
+    // Given
+    const buttons = template.querySelectorAll('[data-test="button"]');
+    const texts = template.querySelectorAll('[data-test="text"]');
+
+    // Then
+    expect(buttons.length).toEqual(component.buttons.length);
+
+    expect(texts[0].textContent.trim()).toBe(component.buttons[0].text);
+    expect(texts[1].textContent.trim()).toBe(component.buttons[1].text);
+    expect(texts[2].textContent.trim()).toBe(component.buttons[2].text);
+    expect(texts[3].textContent.trim()).toBe(component.buttons[3].text);
+  });
+
+  it('should display titulo', () => {
+    // Given
+    const titulo = template.querySelectorAll('[data-test="titulo"]')[0]
+      .textContent;
+    // Then
+    expect(titulo).toEqual(component.titulo);
+  });
+
+  it('should display subtitulo', () => {
+    // Given
+    const subtitulo = template.querySelectorAll('[data-test="subtitulo"]')[0]
+      .textContent;
+
+    // Then
+    expect(subtitulo).toEqual(component.subtitulo);
+  });
+});
