8fc39cb385ee0ca3556c162660ff40e32862d976
Iago Cortes <iagocortes@gmail.com>
2021-08-26 16:38:47 -0300

    feat: ajustes na documentação e aumento de budget

--- packages/ng-syz/projects/ng-syz/src/lib/resultado-simulacao/resultado-simulacao.component.ts
+++ packages/ng-syz/projects/ng-syz/src/lib/resultado-simulacao/resultado-simulacao.component.ts
@@ -15,35 +15,17 @@ export class NgSyzResultadoSimulacaoComponent {
   @Input() valor: number;
   @Input() modalidades: string[];
   @Input() condicoes: NgSyzResultadoSimulacaoCondicao[];
-  @Input() corCardSelecionado = '#0169b3';
-  @Input() corLinhaSelecionada = '#dfeaf2';
-  @Input() corBotaoClicado = '#dc7700';
   @Output() enviarCardSelecionado = new EventEmitter(true);
   @Output() enviarModalidadeSelecionada = new EventEmitter(true);
   cardIdSelecionadoAnteriormente: string;
 
-  constructor() {
-    document.documentElement.style.setProperty(
-      '--selected-card',
-      this.corCardSelecionado
-    );
-    document.documentElement.style.setProperty(
-      '--selected-line',
-      this.corLinhaSelecionada
-    );
-    document.documentElement.style.setProperty(
-      '--selected-button',
-      this.corBotaoClicado
-    );
-  }
-
-  selecionarCard(index): void {
+  selecionarCard(index, idLetter): void {
     if (this.cardIdSelecionadoAnteriormente) {
       document
         .getElementById(this.cardIdSelecionadoAnteriormente)
         .classList.remove('card-selecionado');
     }
-    const id = `card${index}`;
+    const id = `card${idLetter + index}`;
     document.getElementById(id).classList.add('card-selecionado');
     this.cardIdSelecionadoAnteriormente = id;
     this.enviarCardSelecionado.emit(index);
