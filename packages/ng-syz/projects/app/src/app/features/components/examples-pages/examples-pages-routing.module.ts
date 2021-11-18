a7668711931213823fc2a36abf607afb8b69b36f
Touré Holder <toureh@hotmail.com>
2021-11-18 18:05:53 -0300

    chore(ng-syz): ajustar roteamento de exemplos de página inteira

--- packages/ng-syz/projects/app/src/app/features/components/examples-pages/examples-pages-routing.module.ts
+++ packages/ng-syz/projects/app/src/app/features/components/examples-pages/examples-pages-routing.module.ts
@@ -3,42 +3,46 @@ import { RouterModule, Routes } from '@angular/router';
 import { ExamplesPagesComponent } from './examples-pages.component';
 
 const routes: Routes = [
-  { path: '', component: ExamplesPagesComponent },
-
   {
-    path: 'login-with-carousel-flow-example-one',
-    loadChildren: () =>
-      import(
-        './login-with-carousel-flow-example-one/login-with-carousel-flow-example-one.module'
-      ).then((m) => m.LoginWithCarouselFlowExampleOneModule),
-  },
-  {
-    path: 'login-with-carousel-flow-example-two',
-    loadChildren: () =>
-      import(
-        './login-with-carousel-flow-example-two/login-with-carousel-flow-example-two.module'
-      ).then((m) => m.LoginWithCarouselFlowExampleTwoModule),
-  },
-  {
-    path: 'login-with-cards-flow-example-one',
-    loadChildren: () =>
-      import(
-        './login-with-cards-flow-example-one/login-with-cards-flow-example-one.module'
-      ).then((m) => m.LoginWithCardsFlowExampleOneModule),
-  },
-  {
-    path: 'login-with-cards-flow-example-two',
-    loadChildren: () =>
-      import(
-        './login-with-cards-flow-example-two/login-with-cards-flow-example-two.module'
-      ).then((m) => m.LoginWithCardsFlowExampleTwoModule),
-  },
-  {
-    path: 'button-grid-example-one',
-    loadChildren: () =>
-      import('./button-grid-example-one/button-grid-example-one.module').then(
-        (m) => m.ButtonGridExampleOneModule
-      ),
+    path: '',
+    component: ExamplesPagesComponent,
+    children: [
+      {
+        path: 'login-with-carousel-flow-example-one',
+        loadChildren: () =>
+          import(
+            './login-with-carousel-flow-example-one/login-with-carousel-flow-example-one.module'
+          ).then((m) => m.LoginWithCarouselFlowExampleOneModule),
+      },
+      {
+        path: 'login-with-carousel-flow-example-two',
+        loadChildren: () =>
+          import(
+            './login-with-carousel-flow-example-two/login-with-carousel-flow-example-two.module'
+          ).then((m) => m.LoginWithCarouselFlowExampleTwoModule),
+      },
+      {
+        path: 'login-with-cards-flow-example-one',
+        loadChildren: () =>
+          import(
+            './login-with-cards-flow-example-one/login-with-cards-flow-example-one.module'
+          ).then((m) => m.LoginWithCardsFlowExampleOneModule),
+      },
+      {
+        path: 'login-with-cards-flow-example-two',
+        loadChildren: () =>
+          import(
+            './login-with-cards-flow-example-two/login-with-cards-flow-example-two.module'
+          ).then((m) => m.LoginWithCardsFlowExampleTwoModule),
+      },
+      {
+        path: 'button-grid-example-one',
+        loadChildren: () =>
+          import(
+            './button-grid-example-one/button-grid-example-one.module'
+          ).then((m) => m.ButtonGridExampleOneModule),
+      },
+    ],
   },
 ];
 
